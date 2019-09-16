9b97ae80591371fa9e9f7a8ecc6f60e9f828698d
Simon O'Donnell <simonrodonnell@yahoo.co.uk>
2019-09-16 14:55:30 +0100

    Part B refactored

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -6,23 +6,62 @@ class TestSportsTeam < MiniTest::Test
 
   def test_get_sports_team_name
     sports_team = SportsTeam.new("Code Clan Utd", ["Fat Ronaldo", "Portuguese Ronaldo"], "Gaffer McGafferson")
-    assert_equal("Code Clan Utd", sports_team.get_team_name())
+    assert_equal("Code Clan Utd", sports_team.team_name())
   end
 
   def test_get_players
     sports_team = SportsTeam.new("Code Clan Utd", ["Fat Ronaldo", "Portuguese Ronaldo"], "Gaffer McGafferson")
-    assert_equal(["Fat Ronaldo", "Portuguese Ronaldo"], sports_team.get_players())
+    assert_equal(["Fat Ronaldo", "Portuguese Ronaldo"], sports_team.players())
   end
 
   def test_get_coach_name
     sports_team = SportsTeam.new("Code Clan Utd", ["Fat Ronaldo", "Portuguese Ronaldo"], "Gaffer McGafferson")
-    assert_equal("Gaffer McGafferson", sports_team.get_coach_name())
+    assert_equal("Gaffer McGafferson", sports_team.coach_name())
   end
 
   def test_set_coach_name
     sports_team = SportsTeam.new("Code Clan Utd", ["Fat Ronaldo", "Portuguese Ronaldo"], "Gaffer McGafferson")
-    sports_team.set_coach_name("Gafferoni DiGaffersoni")
-    assert_equal("Gafferoni DiGaffersoni", sports_team.get_coach_name())
+    sports_team.coach_name = "Gafferoni DiGaffersoni"
+    assert_equal("Gafferoni DiGaffersoni", sports_team.coach_name())
+  end
+
+  def test_add_new_player
+    sports_team = SportsTeam.new("Code Clan Utd", ["Fat Ronaldo", "Portuguese Ronaldo"], "Gaffer McGafferson")
+    sports_team.add_new_player("Nakamura")
+    assert_equal(["Fat Ronaldo", "Portuguese Ronaldo", "Nakamura"], sports_team.players())
+  end
+
+  def test_find_player__is_true
+    sports_team = SportsTeam.new("Code Clan Utd", ["Fat Ronaldo", "Portuguese Ronaldo"], "Gaffer McGafferson")
+    assert_equal(true, sports_team.find_player("Fat Ronaldo"))
+  end
+
+  def test_find_player__is_false
+    sports_team = SportsTeam.new("Code Clan Utd", ["Fat Ronaldo", "Portuguese Ronaldo"], "Gaffer McGafferson")
+    assert_equal(false, sports_team.find_player("Roy of the Rovers"))
+  end
+
+  def test_get_points
+    sports_team = SportsTeam.new("Code Clan Utd", ["Fat Ronaldo", "Portuguese Ronaldo"], "Gaffer McGafferson")
+    assert_equal(0, sports_team.points())
+  end
+
+  def test_update_points__3_points_for_a_win
+    sports_team = SportsTeam.new("Code Clan Utd", ["Fat Ronaldo", "Portuguese Ronaldo"], "Gaffer McGafferson")
+    sports_team.update_points("win")
+    assert_equal(3, sports_team.points())
+  end
+
+  def test_update_points__1_points_for_a_draw
+    sports_team = SportsTeam.new("Code Clan Utd", ["Fat Ronaldo", "Portuguese Ronaldo"], "Gaffer McGafferson")
+    sports_team.update_points("draw")
+    assert_equal(1, sports_team.points())
+  end
+
+  def test_update_points__0_points_for_a_loss
+    sports_team = SportsTeam.new("Code Clan Utd", ["Fat Ronaldo", "Portuguese Ronaldo"], "Gaffer McGafferson")
+    sports_team.update_points("loss")
+    assert_equal(0, sports_team.points())
   end
 
 end
--- sports_team.rb
+++ sports_team.rb
@@ -1,25 +1,48 @@
 class SportsTeam
 
+  attr_accessor(:team_name, :players, :coach_name, :points)
+
   def initialize(input_team_name, input_players, input_coach_name)
     @team_name = input_team_name
     @players = input_players
     @coach_name = input_coach_name
+    @points = 0
   end
 
-  def get_team_name()
-    return @team_name
+  # def get_team_name()
+  #   return @team_name
+  # end
+  #
+  # def get_players()
+  #   return @players
+  # end
+  #
+  # def get_coach_name()
+  #   return @coach_name
+  # end
+  #
+  # def set_coach_name(new_coach)
+  #   @coach_name = new_coach
+  # end
+
+  def add_new_player(new_player)
+    @players.push(new_player)
   end
 
-  def get_players()
-    return @players
+  def find_player(search_name)
+    for player in @players
+      return true if player == search_name
+    end
+    return false
   end
 
-  def get_coach_name()
-    return @coach_name
-  end
-
-  def set_coach_name(new_coach)
-    @coach_name = new_coach
+  def update_points(result)
+    points_array = {
+      "win" => 3,
+      "draw" => 1,
+      "loss" => 0
+    }
+    @points += points_array[result]
   end
 
 end
