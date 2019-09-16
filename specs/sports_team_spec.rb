7d891a8156e3dde2f7028c91da355e97b8b6ab56
Simon O'Donnell <simonrodonnell@yahoo.co.uk>
2019-09-16 14:31:17 +0100

    Part B with getter and setters

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -4,4 +4,25 @@ require_relative("../sports_team")
 
 class TestSportsTeam < MiniTest::Test
 
+  def test_get_sports_team_name
+    sports_team = SportsTeam.new("Code Clan Utd", ["Fat Ronaldo", "Portuguese Ronaldo"], "Gaffer McGafferson")
+    assert_equal("Code Clan Utd", sports_team.get_team_name())
+  end
+
+  def test_get_players
+    sports_team = SportsTeam.new("Code Clan Utd", ["Fat Ronaldo", "Portuguese Ronaldo"], "Gaffer McGafferson")
+    assert_equal(["Fat Ronaldo", "Portuguese Ronaldo"], sports_team.get_players())
+  end
+
+  def test_get_coach_name
+    sports_team = SportsTeam.new("Code Clan Utd", ["Fat Ronaldo", "Portuguese Ronaldo"], "Gaffer McGafferson")
+    assert_equal("Gaffer McGafferson", sports_team.get_coach_name())
+  end
+
+  def test_set_coach_name
+    sports_team = SportsTeam.new("Code Clan Utd", ["Fat Ronaldo", "Portuguese Ronaldo"], "Gaffer McGafferson")
+    sports_team.set_coach_name("Gafferoni DiGaffersoni")
+    assert_equal("Gafferoni DiGaffersoni", sports_team.get_coach_name())
+  end
+
 end
